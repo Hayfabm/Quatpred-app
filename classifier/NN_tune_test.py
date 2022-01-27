21c16f3c96f6c1646b4c20979673d517e2808526
Hayfabm <haifa.b.messaoud@gmail.com>
2022-01-27 11:23:33 +0000

    ini all

---
+++ classifier/NN_tune_test.py
@@ -0,0 +1,210 @@
+import datetime
+import os
+import random
+
+import neptune.new as neptune
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from keras.utils import np_utils
+from neptune.new.integrations.tensorflow_keras import NeptuneCallback
+from protlearn.features import aaindex1, paac
+from sklearn.model_selection import StratifiedKFold, train_test_split
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.callbacks import (EarlyStopping, ModelCheckpoint,
+                                        ReduceLROnPlateau)
+from tensorflow.keras.layers import Dense, Dropout, Input
+from tensorflow.keras.models import Model
+
+from features_extraction import CT_processing, DPC_processing
+from utils import (calculate_performace, categorical_probas_to_classes,
+                   create_dataset)
+
+# Model architecture
+
+input_1 = Input(shape=(1319)) #imput dimension
+dense11 = Dense(100, activation="relu")(input_1)
+dense12 = (Dense(50, activation='relu'))(dense11)
+drop2 = Dropout(0.1)(dense12)
+output = Dense(17, activation="softmax")(drop2)
+model = Model(inputs=[input_1], outputs=output)
+# summarize layers
+print(model.summary())
+
+
+if __name__ == "__main__":
+
+
+
+    # init neptune logger
+    run = neptune.init(
+         project="sophiedalentour/QuaPred", 
+         tags=["DPC", "PseAAC", "CT", "aaind", "muti_class_ classification"],
+        )
+
+    # set the seed
+    SEED = 42
+    os.environ["PYTHONHASHSEED"] = str(SEED)
+    random.seed(SEED)
+    np.random.seed(SEED)
+    tf.random.set_seed(SEED)
+    
+    
+    # training parameters
+    BATCH_SIZE = 128
+    NUM_EPOCHS = 30
+    SAVED_MODEL_PATH = (
+        "logs/model_" + datetime.datetime.now().strftime("%Y%m%d%H%M%S") + ".hdf5"
+    )
+    DATA = "Uniprot_data/uniprot_dataset.csv"
+    
+  
+    # save parameters in neptune
+    run["hyper-parameters"] = {
+        "encoding_mode": "bio-transformers",
+        "seed": SEED,
+        "batch_size": BATCH_SIZE,
+        "num_epochs": NUM_EPOCHS,
+        "saved_model_path": SAVED_MODEL_PATH,
+        "data": DATA,
+    }
+
+    # build model
+     # compile model
+    model.compile(
+        loss="categorical_crossentropy",
+        optimizer="adam",
+        metrics=["accuracy", "AUC", "Precision", "Recall"],
+    )
+
+    # create  dataset
+    sequences, labels = create_dataset(data_path=DATA)
+
+    ### feature encodings
+    # DPC 
+    dip = DPC_processing(sequences)
+    dipeptide= np.array(dip)
+    print(dipeptide.shape) #(, 400)
+	
+    
+    # PseAAC 
+    seqs = sequences
+    paac_comp, desc = paac(seqs, lambda_=3, remove_zero_cols=True)
+    print(paac_comp.shape) #(, 23)
+    
+    # CT 
+    CT = CT_processing(sequences)
+    conjoint_triad= np.array(CT)
+    print(conjoint_triad.shape) #(, 343)
+
+
+    # AAindex 
+    seqs = sequences
+    aaind, inds = aaindex1(seqs, standardize='zscore')
+    print(aaind.shape) #(, 553)
+    
+    # Dimension concatenation
+    concat= np.concatenate(
+        (
+        
+        dipeptide,
+        paac_comp,
+        conjoint_triad,
+        aaind
+        ), axis= 1
+    )
+    print(concat.shape) #(, 423)
+    
+
+    # encode class values as integers
+    encoder = LabelEncoder()
+    encoder.fit(labels)
+    encoded_labels = encoder.transform(labels)
+    print(encoded_labels)
+    # convert integers to dummy variables (i.e. one hot encoded)
+    one_hot_encoded = np_utils.to_categorical(encoded_labels)
+    print(one_hot_encoded) #(,17)
+
+    # split data 
+    x_train, X_test , y_train, Y_test = train_test_split(
+                concat,
+                encoded_labels, 
+                test_size=0.1,
+            )
+    
+    k_fold = 10
+    scores = []
+    skf = StratifiedKFold(n_splits=k_fold, shuffle=True, random_state=1024)
+    for ((train, valid), k) in zip(skf.split(x_train, y_train), range(k_fold)):
+        X_train = concat[train]
+        print(X_train.shape)
+        X_val = concat[valid]
+        print(X_val.shape)
+        y_tr = encoded_labels[train]
+        y_train= np_utils.to_categorical(y_tr)
+        y_te = encoded_labels[valid]
+        y_val= np_utils.to_categorical(y_te)
+        print(y_val)
+        
+        # define callbacks
+        my_callbacks = [
+            # ReduceLROnPlateau(monitor="val_loss", factor=0.1, patience=3, verbose=1),
+            # EarlyStopping(monitor="val_loss", min_delta=0, patience=5, verbose=1),
+            ModelCheckpoint(
+                monitor="val_accuracy",
+                mode="max",
+                filepath=SAVED_MODEL_PATH,
+                save_best_only=True,
+            ),
+            NeptuneCallback(run=run, base_namespace="metrics"),
+        ]
+
+        # fit the model
+        history = model.fit(
+            X_train, y_train,
+            batch_size=BATCH_SIZE,
+            epochs=NUM_EPOCHS,
+            verbose=1,
+            validation_data= (X_val, y_val)
+            ,
+            callbacks=my_callbacks,
+        )
+    
+    # prediction probability in test set 
+    print(X_test)
+    print(X_test.shape)
+    predictions = model.predict(X_test)
+    print(predictions)
+    y_class = categorical_probas_to_classes(predictions)
+    
+    print(y_class)
+    reverse = encoder.inverse_transform(y_class)
+    print(reverse)
+    score = np.argmax(predictions)
+    print(score)
+    
+    
+    # true_y_C_C=utils.categorical_probas_to_classes(true_y_C)
+    y_test= np_utils.to_categorical(Y_test)
+
+    true_y = categorical_probas_to_classes(y_test)
+    (   
+        sensitivity, 
+        specificity,
+        precision,
+        acc,
+        mcc,
+    ) = calculate_performace(len(y_class), y_class, true_y)
+    print("======================")
+    print("======================")
+    print(
+        "Iter " + ", " + str(k + 1) + " of " + str(k_fold) + "cv:"
+    )
+    print(
+        "\tsn='%0.4f', sp='%0.4f', precision='%0.4f',  acc='%0.4f', mcc='%0.4f'"
+        % (sensitivity, specificity, precision, acc, mcc)
+    )
+    
+    
+run.stop()
+
